444c7963d0189386008bf9b1bd78004555d721d0
0931REB462 <125568233+0931REB462@users.noreply.github.com>
2023-03-18 18:43:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,62 @@
 # python3
 
-import heapq
 
+from collections import namedtuple
 
-class Worker:
-        def __init__(self, thread_id, release_time=0):
-        self.thread_id = thread_id
-        self.release_time = release_time
-
-    def __lt__(self, other):
-        if self.release_time == other.release_time:
-            return self.thread_id < other.thread_id
-        return self.release_time < other.release_time
-
-    def __gt__(self, other):
-        if self.release_time == other.release_time:
-            return self.thread_id > other.thread_id
-        return self.release_time > other.release_time
+AssignedJob = namedtuple('AssignedJob', ['worker', 'started_at'])
 
 
 class JobQueue:
+    def __init__(self, n_workers, jobs):
+        self.n = n_workers
+        self.jobs = jobs
+        self.finish_time = []
+        self.assigned_jobs = []
+        for i in range(self.n):
+            self.finish_time.append([i, 0])
 
-    def read_data(self):
-        """Reads data from standard input."""
-        self.num_workers, m = map(int, input().split())
-        self.jobs = list(map(int, input().split()))
-        self.size = len(self.jobs)
-        assert m == self.size
+    def SiftDown(self, i):
+        min_index = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < self.n:
+            if self.finish_time[min_index][1] > self.finish_time[left][1]:
+                min_index = left
+            elif self.finish_time[min_index][1] == self.finish_time[left][1]:
+                if self.finish_time[min_index][0] > self.finish_time[left][0]:
+                    min_index = left
+        if right < self.n:
+            if self.finish_time[min_index][1] > self.finish_time[right][1]:
+                min_index = right
+            elif self.finish_time[min_index][1] == self.finish_time[right][1]:
+                if self.finish_time[min_index][0] > self.finish_time[right][0]:
+                    min_index = right
+        if min_index != i:
+            self.finish_time[i], self.finish_time[min_index] = self.finish_time[min_index], self.finish_time[i]
+            self.SiftDown(min_index)
 
-    def write_response(self):
-        """Writes the response to standard output."""
-        for worker_id, start_time in self.result:
-            print(worker_id, start_time)
+    def NextWorker(self, job):
+        root = self.finish_time[0]
+        next_worker = root[0]
+        started_at = root[1]
+        self.assigned_jobs.append(AssignedJob(next_worker,started_at))
+        self.finish_time[0][1] += job
+        self.SiftDown(0)
 
-    def assign_jobs(self):
-        """Assigns jobs to corresponding workers"""
-        self.result = []
-        self.worker_queue = [Worker(i) for i in range(self.num_workers)]
 
-        for job in self.jobs:
-            worker = heapq.heappop(self.worker_queue)
+def main():
+    n_workers, n_jobs = map(int, input().split())
+    jobs = list(map(int, input().split()))
+    assert len(jobs) == n_jobs
 
-            self.result.append((worker.thread_id, worker.release_time))
+    job_queue = JobQueue(n_workers, jobs)
+    for job in jobs:
+        job_queue.NextWorker(job)
+    assigned_jobs = job_queue.assigned_jobs
 
-            worker.release_time += job
-            heapq.heappush(self.worker_queue, worker)
-
-    def solve(self):
-        self.read_data()
-        self.assign_jobs()
-        self.write_response()
+    for job in assigned_jobs:
+        print(job.worker, job.started_at)
 
 
 if __name__ == "__main__":
-    job_queue = JobQueue()
-    job_queue.solve()
+    main()
